c31ec3f2d543b3c2c1c6a58387efc348099e069c
root <research.csh@gmail.com>
2020-09-05 21:51:02 -0700

    test

---
+++ code.py
@@ -0,0 +1,18 @@
+import subprocess, getpass, os
+from pwd import getpwnam
+from utils.exception import CgroupsException,BASE_CGROUPS
+from core.user import create_cgroups, get_user_info
+import logging
+
+CONTROL = [
+    'cpu',
+    'memory',
+]
+
+logger = logging.getLogger(__name__)
+CPU_DEFAULT = 1024
+MEMORY_DEFAULT = 1
+
+
+class Cgroup(object):
+    def __init__(self, cgroups_name, target='all', user='syscore'):
