280640c419530fc29ca1f0fac8df72150fb5cf74
root <research.csh@gmail.com>
2020-10-01 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,121 +1,36 @@
-import subprocess, getpass, os
-from pwd import getpwnam
-from utils.exception import CgroupsException,BASE_CGROUPS
-from core.user import create_cgroups, get_user_info
-import logging
 
-CONTROL = [
-    'cpu',
-    'memory',
-]
+import subprocess,threading,time
 
-logger = logging.getLogger(__name__)
-CPU_DEFAULT = 1024
-MEMORY_DEFAULT = 1
+import os,signal
+from utils.parsing import finder
+import json
+from utils.out import bold_print,u_print,analysis
 
 
-class Cgroup(object):
-    def __init__(self, cgroups_name, target='all', user='syscore'):
-        self.cgroups_name = cgroups_name
-        self.user = user
-        self.target = ''
-        self.u_cgroups = {}
-        self.target = [c for c in target if c in CONTROL]
-        system = os.listdir(BASE_CGROUPS)
-
-        for h in self.target:  #
-            if h not in system:
-                exit(-1)
-            user_cgroup = os.path.join(BASE_CGROUPS, h, self.user)
-            self.u_cgroups[h] = user_cgroup
-        create_cgroups(cgroups_name, script=False)
-        self.cgroups = {}
-
-        for h, user_cgroup in self.u_cgroups.items():
-            cgroup = os.path.join(user_cgroup, self.cgroups_name)
-            if not os.path.exists(cgroup):
-                os.mkdir(cgroup)
-            self.cgroups[h] = cgroup
-
-    def _get_file(self, hierarchy, file_name):
-        return os.path.join(self.cgroups[hierarchy], file_name)
-
-    def set_cpu_limit(self, limit=None):
-        if 'cpu' in self.cgroups:
-            value = self._format_cpu_value(limit)
-            cpu_shares_file = self._get_file('cpu', 'cpu.shares')
-            with open(cpu_shares_file, 'w+') as f:
-                f.write('%s\n' % value)
-        else:
-            exit(-1)
-
-    #memory
-    def get_memory_info(self,app_name,info):
-        usage = 0
-        with open('/sys/fs/cgroup/memory/' + str(app_name) + '/memory.'+info, 'r+') as f:
-            data = f.readline()
-            usage = int(data)
-        return usage
-
-    def get_memory_stat(self,app_name,info):
-        f=open('/sys/fs/cgroup/memory/' + str(app_name) + '/memory.stat', 'r+')
-        lines=f.readlines()
-        for line in lines:
-            line=line.strip('\n')
-            data=line.split(' ')
-            if info==data[0] :
-                return int(data[1])
-
-
-    # CPU
-    def _format_cpu_value(self, limit=None):
-        if limit is None:
-            value = CPU_DEFAULT
-        else:
-            limit = limit / 100
-            value = int(round(CPU_DEFAULT * limit))
-        return value
-
-    def set_memory_limit(self, limit=None, unit='megabytes'):
-        if 'memory' in self.cgroups:
-            value = self._format_memory_value(unit, limit)
-            memory_limit_file = self._get_file(
-                'memory', 'memory.limit_in_bytes')
-
-            with open(memory_limit_file, 'w+') as f:
-                f.write('%s\n' % value)
-        else:
-            exit(-1)
-
-    def _format_memory_value(self, unit, limit=None):
-        units = ('b', 'kb', 'mb', 'gb')
-        if unit not in units:
-            exit(-1)
-        if limit is None:
-            value = MEMORY_DEFAULT
-        else:
-            try:
-                limit = int(limit)
-            except ValueError:
-                exit(-1)
-            else:
-                if unit == 'b':
-                    value = limit
-                elif unit == 'kb':
-                    value = limit * 1024
-                elif unit == 'mb':
-                    value = limit * 1024 * 1024
-                elif unit == 'gb':
-                    value = limit * 1024 * 1024 * 1024
-        return value
-
-    def sleep_memory(self):  # Memory 잠들게 하기
-        if 'memory' in self.cgroups:
-            value = 1024 * 10
-            memory_limit_file = self._get_file(
-                'memory', 'memory.limit_in_bytes')
-            with open(memory_limit_file, 'w+') as f:
-                f.write('%s\n' % value)
-        else:
-            exit(-1)
-    def sleep_cpu(self):
+def dy_tracing_analysis():
+    msg="\n\n"+"*"*10+" 4. Dynamic Lib 에 대한 연계 분석 "+"*"*10
+    bold_print(msg)
+    lib_dir='analysis'
+    file_list = os.listdir(lib_dir)
+    node_pool=[]
+    json_list=[]
+    for f_name in file_list:
+        if finder(f_name, '.dy_trace.result'):
+            with open(lib_dir + "/" + f_name) as json_file:
+                json_data = json.load(json_file)
+                for node in json_data:
+                    if not str(node) in node_pool:
+                        node_pool.append(node)
+                json_list.append(f_name)
+        # msg = '동적 라이브러리 분석 결과\n'+'-'*100+'\n'
+    app_list=[]
+    for app in json_list:
+        app_list.append(app.replace(".dy_trace.result",''))
+    msg = "{0} 이 호출한 Dynamic Lib  갯수는 총 {1} 개입니다.".format(app_list, len(node_pool))  # 최종수정 다시해보기 100%라고 나옴 b.py이상한앤데..
+    u_print(msg)
+    msg = '-' * 100
+    u_print(msg)
+    u_print('dy call list:'+str(node_pool))
+    for target in range(0,len(json_list)-1):
+        for a_target in range(target+1, len(json_list)):
+            base_file=(json_list[target])
