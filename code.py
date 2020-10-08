68f7f9aef199de713fecd924cfd583f17afaf553
root <research.csh@gmail.com>
2020-10-07 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,34 +1,3 @@
-#! /usr/python3
-# -*- coding:utf-8 -*-
-
-import os,sys
-import logging
-from pwd import getpwnam
-
-from utils.exception import CgroupsException,BASE_CGROUPS
-
-logger = logging.getLogger(__name__)
-
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logstream = logging.StreamHandler()
-logstream.setFormatter(formatter)
-logger.addHandler(logstream)
-logger.setLevel(logging.INFO)
-def log(type,msg):
-    if type=='INFO':
-        logger.info('{0}'.format(msg))
-    elif type=='WARN':
-        logger.info('{0}'.format(msg))
-
-def get_user_info(user):
-    try:
-        user_system = getpwnam(user)
-    except KeyError:
-        raise CgroupsException("User %s doesn't exists" % user)
-    else:
-        uid = user_system.pw_uid
-        gid = user_system.pw_gid
-    return uid, gid
-
+import os
+from utils.out import info,critical
 
