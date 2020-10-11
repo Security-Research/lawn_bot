83a72c098ea3733533343b29310039bba5ce9963
root <research.csh@gmail.com>
2020-10-10 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,18 +1,6 @@
-import argparse
+#! /usr/python3
+# -*- coding:utf-8 -*-
 
-from core.report import report
-from core.manager import reset
-from core.execute import execute
-
-
-def isint(a):
-
-    if int(a):
-        return 1
-    else:
-        return 0
-
-def get_arguments():
-    return [
-    ("--start", "Start a Thermometer deamon"),
-    ("--reporter", "Get a reporter"),
+import os,sys
+import logging
+from pwd import getpwnam
