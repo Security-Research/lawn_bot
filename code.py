54bf9c0b00a7dbefc7681ea556c4036e082fd44d
root <research.csh@gmail.com>
2020-10-18 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,9 +1,10 @@
 import os
-import subprocess
-support_interpreter='/usr/bin/python3'
-import threading
-import time
-from utils.out import info,warning,critical
-from core.lib_analysis import get_lib
-from core.tracing import tracing
-from core.dy_tracing import ltracing
+from utils.out import info,critical
+
+
+
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+
+            os.mkdir(directory)
