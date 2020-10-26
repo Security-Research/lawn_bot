093aa196755e988eab9df1112a155a04b57a2366
root <research.csh@gmail.com>
2020-10-25 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,14 +1,1 @@
-import os
-from utils.out import info,critical
-
-
-
-def createFolder(directory):
-    try:
-        if not os.path.exists(directory):
-
-            os.mkdir(directory)
-            info("Created directory",directory)
-    except OSError:
-        print('Error: Creating directory. ' + directory)
 
