90c87b128af155d45b22882c26f575f5b9b79e35
root <research.csh@gmail.com>
2020-11-03 20:51:02 -0800

    test

--- code.py
+++ code.py
@@ -1,5 +1,8 @@
-import os
-import subprocess
-support_interpreter='/usr/bin/python3'
-import threading
+from ctypes import *
 import time
+while(True):
+
+    #load the shared object file
+    adder = CDLL('./testing_app/adder.so')
+
+    #Find sum of integers
