b55cd25044fe652659d14b275a7cac6500909539
root <research.csh@gmail.com>
2020-11-09 20:51:02 -0800

    test

--- code.py
+++ code.py
@@ -1,24 +1,8 @@
 import os
-import subprocess
-support_interpreter='/usr/bin/python3'
-import threading
-import time
-from utils.out import info,warning,critical
-from core.lib_analysis import get_lib
-from core.tracing import tracing
-from core.dy_tracing import ltracing
-kill_time=10
-
-def poll(target,pg):
-    #warning("Analysis",""+str(target))
-    while pg.poll() == None:
-        out = pg.stdout.readline()
-        time.sleep(kill_time)
-        #critical("kill",pg.pid)
-        #pg.kill()
-        break
+from utils.out import info,critical
 
 
-def run_app(target,sec):
-    kill_time=sec
-    cmd=support_interpreter+' testing_app/'+str(target)
+
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
