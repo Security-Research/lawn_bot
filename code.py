162f210559bc0a01e91e7b2c85152f7b8bcce897
root <research.csh@gmail.com>
2020-11-10 20:51:02 -0800

    test

--- code.py
+++ code.py
@@ -1,8 +1,22 @@
-import os
-from utils.out import info,critical
+import argparse
+
+from core.report import report
+from core.manager import reset
+from core.execute import execute
 
 
+def isint(a):
 
-def createFolder(directory):
-    try:
-        if not os.path.exists(directory):
+    if int(a):
+        return 1
+    else:
+        return 0
+
+def get_arguments():
+    return [
+    ("--start", "Start a Thermometer deamon"),
+    ("--reporter", "Get a reporter"),
+    ("--reset","Reset a Thermometer deamon")
+    ]
+
+
