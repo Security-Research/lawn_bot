20af115fb5acc39b8dc01fea289d60c684cc74e3
root <research.csh@gmail.com>
2020-11-13 20:51:02 -0800

    test

--- code.py
+++ code.py
@@ -1,3 +1,22 @@
-
 import os
-def get_app_list():
+from utils.out import info,critical
+
+
+
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+
+            os.mkdir(directory)
+            info("Created directory",directory)
+    except OSError:
+        print('Error: Creating directory. ' + directory)
+
+def removeFolder(directory):
+    try:
+        if os.path.exists(directory):
+            os.rmdir(directory)
+            critical("Remove directory", directory)
+    except:
+        print('Error: Removing directory. ' + directory)
+def chmod():
