d3f8fcb7a9e111b45af844380992ba75c67f2a43
root <research.csh@gmail.com>
2020-12-12 20:51:02 -0800

    test

--- code.py
+++ code.py
@@ -1,17 +1,9 @@
-import threading
-import os
-import time
-import json
-
-
 import os
 from utils.parsing import finder
+from utils.out import bold_print,u_print,critical
 import json
-from utils.out import u_print,analysis,bold_print
-from core.dy_tracing import ltracing
-def tracing_analysis():
-    msg='\n\n'+"*"*10+" 3. System call에 대한 연계 분석 "+"*"*10
-    bold_print(msg)
+def read_analysis(obj,app_name):
     lib_dir='analysis'
-    file_list = os.listdir(lib_dir)
-    node_pool=[]
+    data=''
+    data += '-' * 100 + '\n'
+    if obj == 'res':
