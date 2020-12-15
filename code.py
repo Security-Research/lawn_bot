510efc147837a1e3a7c4d87066e19b3547af6363
root <research.csh@gmail.com>
2020-12-14 20:51:02 -0800

    test

--- code.py
+++ code.py
@@ -1,28 +1,11 @@
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-def u_print(obj):
-    msg="{0}".format(obj)
-    print(bcolors.OKGREEN + msg + bcolors.ENDC)
-
-def bold_print(obj):
-    msg="{0}".format(obj)
-    print(bcolors.BOLD + msg + bcolors.ENDC)
-
-def info(obj,sub):
-    msg="[Info] {0} : {1}".format(obj,sub)
-    print(bcolors.OKBLUE + msg + bcolors.ENDC)
-
-def warning(obj,sub):
-    msg="[Warning] {0} : {1}".format(obj,sub)
-    print(bcolors.WARNING + msg + bcolors.ENDC)
+import os
+import subprocess
+support_interpreter='/usr/bin/python3'
+import threading
+import time
+from utils.out import info,warning,critical
+from core.lib_analysis import get_lib
+from core.tracing import tracing
+from core.dy_tracing import ltracing
+kill_time=10
 
