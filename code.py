126456cc848fb1864b6a60c658681cffa1bc812e
root <research.csh@gmail.com>
2020-12-22 20:51:02 -0800

    test

--- code.py
+++ code.py
@@ -1,16 +1,3 @@
-import os
-import subprocess
-support_interpreter='/usr/bin/python3'
-import threading
-import time
-from utils.out import info,warning,critical
-from core.lib_analysis import get_lib
-from core.tracing import tracing
-from core.dy_tracing import ltracing
-kill_time=10
 
-def poll(target,pg):
-    #warning("Analysis",""+str(target))
-    while pg.poll() == None:
-        out = pg.stdout.readline()
-        time.sleep(kill_time)
+import time
+
