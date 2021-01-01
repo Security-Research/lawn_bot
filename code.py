63d162fc5fc53aa3f15274c14d9d074808995908
root <research.csh@gmail.com>
2020-12-31 20:51:02 -0800

    test

--- code.py
+++ code.py
@@ -1,12 +1,3 @@
 
-import os
-def get_app_list():
-    path_dir='./testing_app'
-    file_list = os.listdir(path_dir)
-    python_list=[]
-    for file_name in file_list:
-        if finder(file_name,'.py') and not finder(file_name,'__init__'):
-            python_list.append(file_name)
-    return (python_list)
-
-def finder(target,ob):
+import time
+for i in range(0,10000000):
