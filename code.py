cc718274bd3b37948d2e8c4ddc61556f40a001d3
root <research.csh@gmail.com>
2021-01-12 20:51:02 -0800

    test

--- code.py
+++ code.py
@@ -1,25 +1,6 @@
-import argparse
 
-from core.report import report
-from core.manager import reset
-from core.execute import execute
+import time
 
-
-def isint(a):
-
-    if int(a):
-        return 1
-    else:
-        return 0
-
-def get_arguments():
-    return [
-    ("--start", "Start a Thermometer deamon"),
-    ("--reporter", "Get a reporter"),
-    ("--reset","Reset a Thermometer deamon")
-    ]
-
-
-def add_arguments():
-    parser=argparse.ArgumentParser(description="Thermometer by syscore")
-    for argument in get_arguments():
+for i in range(0,10000000):
+    time.sleep(0.2)
+    print(i)
