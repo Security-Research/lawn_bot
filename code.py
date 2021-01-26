507675f68780235538684d14ec6cd969efdd1ebd
root <research.csh@gmail.com>
2021-01-25 20:51:02 -0800

    test

---
+++ code.py
@@ -0,0 +1,17 @@
+import argparse
+
+from core.report import report
+from core.manager import reset
+from core.execute import execute
+
+
+def isint(a):
+
+    if int(a):
+        return 1
+    else:
+        return 0
+
+def get_arguments():
+    return [
+    ("--start", "Start a Thermometer deamon"),
