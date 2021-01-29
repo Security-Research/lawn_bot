c0e68b23a2d2f113e1c492b066946ff8d681920b
root <research.csh@gmail.com>
2021-01-28 20:51:02 -0800

    test

--- code.py
+++ code.py
@@ -1,12 +1,10 @@
 import os
-from utils.out import info,critical
-
-
-
-def createFolder(directory):
-    try:
-        if not os.path.exists(directory):
-
-            os.mkdir(directory)
-            info("Created directory",directory)
-    except OSError:
+import subprocess
+support_interpreter='/usr/bin/python3'
+import threading
+import time
+from utils.out import info,warning,critical
+from core.lib_analysis import get_lib
+from core.tracing import tracing
+from core.dy_tracing import ltracing
+kill_time=10
