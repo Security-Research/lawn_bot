aabc8fe843408b52022a59dc58dcf8336c3e5f70
root <research.csh@gmail.com>
2021-02-11 20:51:02 -0800

    test

--- code.py
+++ code.py
@@ -1,9 +1,19 @@
+import json,time
 import os
-from utils.out import info,critical
 
+import subprocess
 
+import os
+import json,time
 
-def createFolder(directory):
-    try:
-        if not os.path.exists(directory):
+lib_dir = ".tmp"
+#file_list = os.listdir(lib_dir)
 
+import os
+import json,time
+from utils.out import warning,u_print,critical,analysis,bold_print
+from utils.parsing import finder
+
+def similarity():
+    msg = '\n\n'+"*" * 10 + " 2. LoadFile 에 대한 연계 분석 " + "*" * 10
+    bold_print(msg)
