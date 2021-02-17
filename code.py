94624463087ef0815734d6d3d5f781235a73a52c
root <research.csh@gmail.com>
2021-02-16 20:51:02 -0800

    test

--- code.py
+++ code.py
@@ -1,8 +1,31 @@
+import argparse
 
-import os
-def get_app_list():
-    path_dir='./testing_app'
-    file_list = os.listdir(path_dir)
-    python_list=[]
-    for file_name in file_list:
-        if finder(file_name,'.py') and not finder(file_name,'__init__'):
+from core.report import report
+from core.manager import reset
+from core.execute import execute
+
+
+def isint(a):
+
+    if int(a):
+        return 1
+    else:
+        return 0
+
+def get_arguments():
+    return [
+    ("--start", "Start a Thermometer deamon"),
+    ("--reporter", "Get a reporter"),
+    ("--reset","Reset a Thermometer deamon")
+    ]
+
+
+def add_arguments():
+    parser=argparse.ArgumentParser(description="Thermometer by syscore")
+    for argument in get_arguments():
+        parser.add_argument(argument[0], help=argument[1], action="store_true")
+    parser.add_argument("-s", action='store', dest='sec', help='running second',default=10)
+
+    return parser.parse_args()
+
+def commands():
