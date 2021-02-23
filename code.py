f87065870bda9e4bde4178f73620cd17fb891095
root <research.csh@gmail.com>
2021-02-22 20:51:02 -0800

    test

--- code.py
+++ code.py
@@ -1,71 +1,65 @@
+import threading
+import os
+import time
+import json
+
+
 import os
 from utils.parsing import finder
-from utils.out import bold_print,u_print,critical
 import json
-def read_analysis(obj,app_name):
-    lib_dir='analysis'
-    data=''
-    data += '-' * 100 + '\n'
-    if obj == 'res':
-        with open('.tmp' + "/" + app_name + '.res.json') as json_file:
-            json_data = json.load(json_file)
-        for v in json_data:
-            name = v
-            msg="{0} - {1}\n".format(name,json_data[name])
-            data+=msg
-    if obj=='file':
-        with open(lib_dir + "/" + app_name+'.lib.result') as json_file:
-            data=json_file.read(1000200000)
-    if obj=='system':
-        with open('.tmp' + "/" + app_name+'.syscall') as json_file:
-            data=json_file.read(1000200000)
-    if obj == 'dy':
-        with open('.tmp' + "/" + app_name + '.ltrace') as json_file:
-            data = json_file.read(1000200000)
-
-    if obj=='all':
-        data += "*" * 10 + " 1.Resource" + "*" * 10+'\n'
-        data+='-'*100+'\n'
-        with open('.tmp' + "/" + app_name + '.res.json') as json_file:
-            json_data = json.load(json_file)
-        for v in json_data:
-            name = v
-            msg="{0} - {1}\n".format(name,json_data[name])
-            data+=msg
-        with open(lib_dir + "/" + app_name+'.lib.result') as json_file:
-            #data+='Loaded File \n'
-            data += "*" * 10 + "\n\n 2.Loaded File " + "*" * 10+'\n'
-            data += '-' * 100 + '\n'
-            data+=json_file.read(1000200000)
-        with open('.tmp' + "/" + app_name+'.syscall') as json_file:
-            data += "*" * 10 + "\n\n 3.System call " + "*" * 10+'\n'
-            data += '-' * 100 + '\n'
-            data+=json_file.read(1000200000)
-        with open('.tmp' + "/" + app_name + '.ltrace') as json_file:
-            #data += 'Dynamic Lib call \n'
-            data += "*" * 10 + "\n\n 4.Dynamic Lib call " + "*" * 10 + '\n'
-            data += '-' * 100 + '\n'
-            data += json_file.read(1000200000)
-    data += '-' * 100 + '\n'
-    print(data)
-def report():
-    lib_dir='./analysis'
-    file_list = os.listdir(lib_dir)
-    app_list=[]
-    for f_name in file_list:
-        if finder(f_name, '.dy_trace.result'):
-            app_name=(f_name.replace(".dy_trace.result",''))
-            app_list.append(app_name)
-    print(app_list)
-    msg = '\n'+"*" * 10 + " 현재 분석된 앱 아래와 같습니다." + "*" * 10
+from utils.out import u_print,analysis,bold_print
+from core.dy_tracing import ltracing
+def tracing_analysis():
+    msg='\n\n'+"*"*10+" 3. System call에 대한 연계 분석 "+"*"*10
     bold_print(msg)
-    i=1
-    for app in (app_list):
-        msg='[{0}]\t{1}'.format(i,app)
-        i+=1
-        u_print(msg)
-    try:
-        select=int(input('분석 할 대상을 선택해주세요: '))
-        msg='선택 된 앱 '+(app_list[int(select)-1])
-        app_name=(app_list[int(select)-1])
-        u_print(msg)
+    lib_dir='analysis'
+    file_list = os.listdir(lib_dir)
+    node_pool=[]
+    json_list=[]
+    for f_name in file_list:
+        if finder(f_name, '.syscall.result'):
+            with open(lib_dir + "/" + f_name) as json_file:
+                json_data = json.load(json_file)
+                for node in json_data:
+                    if not str(node) in node_pool:
+                        node_pool.append(node)
+                json_list.append(f_name)
+        # msg = '동적 라이브러리 분석 결과\n'+'-'*100+'\n'
+    app_list=[]
+    for app in json_list:
+        app_list.append(app.replace(".syscall.result",''))
+    msg = "{0} 이 호출한 syscall 갯수는 총 {1} 개입니다.".format(app_list, len(node_pool))  # 최종수정 다시해보기 100%라고 나옴 b.py이상한앤데..
+    u_print(msg)
+    msg = '-' * 100
+    u_print(msg)
+    u_print('syscall list:'+str(node_pool))
+    for target in range(0,len(json_list)-1):
+        for a_target in range(target+1, len(json_list)):
+            base_file=(json_list[target])
+            target_file=(json_list[a_target])
+            with open(lib_dir + "/" + base_file) as json_file:
+                base_data = json.load(json_file)
+            with open(lib_dir + "/" + target_file) as json_file:
+                target_data = json.load(json_file)
+            count=1
+            target_data_list = []
+            if (len(base_data)) < (len(target_data)):
+                for t in target_data:
+                    target_data_list.append(t)
+                for base in base_data:
+                    if base in (target_data_list):
+                        count += 1
+                percent=count/len(target_data)*100
+            else:
+                for t in base_data:
+                    target_data_list.append(t)
+                for base in target_data:
+                    if base in (target_data_list):
+                        count += 1
+
+                percent=count/len(base_data)*100
+            msg="{0} 과 {1} 앱의 Syscall 유사도는 {2} % 입니다.".format(base_file.replace(".syscall.result",''),target_file.replace(".syscall.result",''),round(percent,2))
+            analysis(msg)
+
+    u_print('-' * 100 + '\n')
+    pass
