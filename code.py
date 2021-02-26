3c424f711bd28d7dfd62eb09fe1d57eddc30a70e
root <research.csh@gmail.com>
2021-02-25 20:51:02 -0800

    test

--- code.py
+++ code.py
@@ -1,26 +1,29 @@
-import os
-from utils.out import info,critical
+import subprocess, getpass, os
+from pwd import getpwnam
+from utils.exception import CgroupsException,BASE_CGROUPS
+from core.user import create_cgroups, get_user_info
+import logging
+
+CONTROL = [
+    'cpu',
+    'memory',
+]
+
+logger = logging.getLogger(__name__)
+CPU_DEFAULT = 1024
+MEMORY_DEFAULT = 1
 
 
+class Cgroup(object):
+    def __init__(self, cgroups_name, target='all', user='syscore'):
+        self.cgroups_name = cgroups_name
+        self.user = user
+        self.target = ''
+        self.u_cgroups = {}
+        self.target = [c for c in target if c in CONTROL]
+        system = os.listdir(BASE_CGROUPS)
 
-def createFolder(directory):
-    try:
-        if not os.path.exists(directory):
-
-            os.mkdir(directory)
-            info("Created directory",directory)
-    except OSError:
-        print('Error: Creating directory. ' + directory)
-
-def removeFolder(directory):
-    try:
-        if os.path.exists(directory):
-            os.rmdir(directory)
-            critical("Remove directory", directory)
-    except:
-        print('Error: Removing directory. ' + directory)
-def chmod():
-    os.system('chmod 777 core/syscall')
-def init():
-    createFolder('.tmp')
-    createFolder('analysis')
+        for h in self.target:  #
+            if h not in system:
+                exit(-1)
+            user_cgroup = os.path.join(BASE_CGROUPS, h, self.user)
