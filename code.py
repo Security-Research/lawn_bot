812a524a6e40555a99e0a5d16f6e99b9caf7dfea
root <research.csh@gmail.com>
2021-03-01 20:51:02 -0800

    test

--- code.py
+++ code.py
@@ -1,34 +1,11 @@
-import argparse
+import subprocess, getpass, os
+from pwd import getpwnam
+from utils.exception import CgroupsException,BASE_CGROUPS
+from core.user import create_cgroups, get_user_info
+import logging
 
-from core.report import report
-from core.manager import reset
-from core.execute import execute
+CONTROL = [
+    'cpu',
+    'memory',
+]
 
-
-def isint(a):
-
-    if int(a):
-        return 1
-    else:
-        return 0
-
-def get_arguments():
-    return [
-    ("--start", "Start a Thermometer deamon"),
-    ("--reporter", "Get a reporter"),
-    ("--reset","Reset a Thermometer deamon")
-    ]
-
-
-def add_arguments():
-    parser=argparse.ArgumentParser(description="Thermometer by syscore")
-    for argument in get_arguments():
-        parser.add_argument(argument[0], help=argument[1], action="store_true")
-    parser.add_argument("-s", action='store', dest='sec', help='running second',default=10)
-
-    return parser.parse_args()
-
-def commands():
-    args = add_arguments()
-    sec=args.sec
-    if args.start:
