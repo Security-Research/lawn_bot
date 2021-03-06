617ac476ce97cbb76c806e3a89417b297b6af485
root <research.csh@gmail.com>
2021-03-05 20:51:02 -0800

    test

--- code.py
+++ code.py
@@ -1,51 +1,52 @@
-import json,time
-import os
+#! /usr/python3
+# -*- coding:utf-8 -*-
 
-import subprocess
+import os,sys
+import logging
+from pwd import getpwnam
 
-import os
-import json,time
+from utils.exception import CgroupsException,BASE_CGROUPS
 
-lib_dir = ".tmp"
-#file_list = os.listdir(lib_dir)
+logger = logging.getLogger(__name__)
 
-import os
-import json,time
-from utils.out import warning,u_print,critical,analysis,bold_print
-from utils.parsing import finder
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logstream = logging.StreamHandler()
+logstream.setFormatter(formatter)
+logger.addHandler(logstream)
+logger.setLevel(logging.INFO)
+def log(type,msg):
+    if type=='INFO':
+        logger.info('{0}'.format(msg))
+    elif type=='WARN':
+        logger.info('{0}'.format(msg))
 
-def similarity():
-    msg = '\n\n'+"*" * 10 + " 2. LoadFile 에 대한 연계 분석 " + "*" * 10
-    bold_print(msg)
+def get_user_info(user):
+    try:
+        user_system = getpwnam(user)
+    except KeyError:
+        raise CgroupsException("User %s doesn't exists" % user)
+    else:
+        uid = user_system.pw_uid
+        gid = user_system.pw_gid
+    return uid, gid
 
-    lib_dir = ".tmp"
-    file_list = os.listdir(lib_dir)
-    node_pool=[]
-    json_list=[]
-    for f_name in file_list:
-        if finder(f_name, '.lib.json'):
-            with open(lib_dir + "/" + f_name) as json_file:
-                json_data = json.load(json_file)
-            for node in json_data:
-                if not str(node) in node_pool:
-                    node_pool.append(node)
-            json_list.append(f_name)
-    #msg = '동적 라이브러리 분석 결과\n'+'-'*100+'\n'
-    app_list=[]
-    for app in json_list:
-        app_list.append(app.replace(".lib.json",''))
-    msg="{0} 이 Load File 갯수는 총 {1} 개입니다.".format(app_list,len(node_pool)) #최종수정 다시해보기 100%라고 나옴 b.py이상한앤데..
-    u_print(msg)
-    msg= '-'*100
-    u_print(msg)
-    #u_print('Load File:' + str(node_pool.))
 
-    for target in range(0,len(json_list)-1):
+def create_cgroups(user, script=True):
+    #logger.info('Creating cgroups sub-directories for  %s' % user)
 
-        for a_target in range(target+1, len(json_list)):
-            base_file=(json_list[target])
-            target_file=(json_list[a_target])
-            with open(lib_dir + "/" + base_file) as json_file:
-                base_data = json.load(json_file)
-            with open(lib_dir + "/" + target_file) as json_file:
-                target_data = json.load(json_file)
+    try:
+        hierarchies = os.listdir(BASE_CGROUPS)
+    except OSError as e:
+        if e.errno == 2:
+            msg = "cgroups filesystem is not mounted on{0}".format(BASE_CGROUPS)  # cgroups 제어 검증 에러
+            raise CgroupsException(msg)
+        else:
+            raise OSError(e)
+    #logger.info('Hierarchies availables: {0}'.format(hierarchies))
+    for hierarchy in hierarchies:
+        user_cgroup = os.path.join(BASE_CGROUPS, hierarchy, user) #경로와 제어 권한 정보를 유저명과 합침
+        #print(user_cgroup)
+        if not os.path.exists(user_cgroup):
+            try:
+                os.mkdir(user_cgroup) #폴더생성
