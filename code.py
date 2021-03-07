ed546a52e33b372383d39e08ff5b8974e84b2a70
root <research.csh@gmail.com>
2021-03-06 20:51:02 -0800

    test

--- code.py
+++ code.py
@@ -1,52 +1,26 @@
-#! /usr/python3
-# -*- coding:utf-8 -*-
 
-import os,sys
-import logging
-from pwd import getpwnam
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-from utils.exception import CgroupsException,BASE_CGROUPS
+def u_print(obj):
+    msg="{0}".format(obj)
+    print(bcolors.OKGREEN + msg + bcolors.ENDC)
 
-logger = logging.getLogger(__name__)
+def bold_print(obj):
+    msg="{0}".format(obj)
+    print(bcolors.BOLD + msg + bcolors.ENDC)
 
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logstream = logging.StreamHandler()
-logstream.setFormatter(formatter)
-logger.addHandler(logstream)
-logger.setLevel(logging.INFO)
-def log(type,msg):
-    if type=='INFO':
-        logger.info('{0}'.format(msg))
-    elif type=='WARN':
-        logger.info('{0}'.format(msg))
+def info(obj,sub):
+    msg="[Info] {0} : {1}".format(obj,sub)
+    print(bcolors.OKBLUE + msg + bcolors.ENDC)
 
-def get_user_info(user):
-    try:
-        user_system = getpwnam(user)
-    except KeyError:
-        raise CgroupsException("User %s doesn't exists" % user)
-    else:
-        uid = user_system.pw_uid
-        gid = user_system.pw_gid
-    return uid, gid
-
-
-def create_cgroups(user, script=True):
-    #logger.info('Creating cgroups sub-directories for  %s' % user)
-
-    try:
-        hierarchies = os.listdir(BASE_CGROUPS)
-    except OSError as e:
-        if e.errno == 2:
-            msg = "cgroups filesystem is not mounted on{0}".format(BASE_CGROUPS)  # cgroups 제어 검증 에러
-            raise CgroupsException(msg)
-        else:
-            raise OSError(e)
-    #logger.info('Hierarchies availables: {0}'.format(hierarchies))
-    for hierarchy in hierarchies:
-        user_cgroup = os.path.join(BASE_CGROUPS, hierarchy, user) #경로와 제어 권한 정보를 유저명과 합침
-        #print(user_cgroup)
-        if not os.path.exists(user_cgroup):
-            try:
-                os.mkdir(user_cgroup) #폴더생성
+def warning(obj,sub):
+    msg="[Warning] {0} : {1}".format(obj,sub)
