6d94547bdce334d126ab271796120563673eae2a
root <research.csh@gmail.com>
2021-03-21 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,5 +1,14 @@
-#! /usr/bin/python3
-# -*- coding:utf-8 -*-
-BASE_CGROUPS = '/sys/fs/cgroup'
-HIDDEN_DIR='.data/'
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def u_print(obj):
+    msg="{0}".format(obj)
