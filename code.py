77239f28a58dda2424f28cfc2fd5e4757384012f
root <research.csh@gmail.com>
2021-04-01 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,34 +1,7 @@
+#! /usr/bin/python3
+# -*- coding:utf-8 -*-
+BASE_CGROUPS = '/sys/fs/cgroup'
+HIDDEN_DIR='.data/'
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-def u_print(obj):
-    msg="{0}".format(obj)
-    print(bcolors.OKGREEN + msg + bcolors.ENDC)
-
-def bold_print(obj):
-    msg="{0}".format(obj)
-    print(bcolors.BOLD + msg + bcolors.ENDC)
-
-def info(obj,sub):
-    msg="[Info] {0} : {1}".format(obj,sub)
-    print(bcolors.OKBLUE + msg + bcolors.ENDC)
-
-def warning(obj,sub):
-    msg="[Warning] {0} : {1}".format(obj,sub)
-    print(bcolors.WARNING + msg + bcolors.ENDC)
-
-
-def critical(obj,sub):
-    msg="[Critical] {0} : {1}".format(obj,sub)
-    print(bcolors.FAIL + msg + bcolors.ENDC)
-
-def analysis(obj):
+class CgroupsException(Exception):
+    if str(Exception).find("Exception")<0:
