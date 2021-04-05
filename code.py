4fe870385b69489e1fd69b9baae98fee368edf82
root <research.csh@gmail.com>
2021-04-04 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,27 +1,10 @@
-#! /usr/python3
-# -*- coding:utf-8 -*-
+import os
+from utils.out import info,critical
 
-import os,sys
-import logging
-from pwd import getpwnam
 
-from utils.exception import CgroupsException,BASE_CGROUPS
 
-logger = logging.getLogger(__name__)
-
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logstream = logging.StreamHandler()
-logstream.setFormatter(formatter)
-logger.addHandler(logstream)
-logger.setLevel(logging.INFO)
-def log(type,msg):
-    if type=='INFO':
-        logger.info('{0}'.format(msg))
-    elif type=='WARN':
-        logger.info('{0}'.format(msg))
-
-def get_user_info(user):
+def createFolder(directory):
     try:
-        user_system = getpwnam(user)
-    except KeyError:
+        if not os.path.exists(directory):
+
+            os.mkdir(directory)
