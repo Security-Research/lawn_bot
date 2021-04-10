3d541118d9b48c7539b0b5db6a711d490fdddfac
root <research.csh@gmail.com>
2021-04-09 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,25 +1,9 @@
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-def u_print(obj):
-    msg="{0}".format(obj)
-    print(bcolors.OKGREEN + msg + bcolors.ENDC)
-
-def bold_print(obj):
-    msg="{0}".format(obj)
-    print(bcolors.BOLD + msg + bcolors.ENDC)
-
-def info(obj,sub):
-    msg="[Info] {0} : {1}".format(obj,sub)
-    print(bcolors.OKBLUE + msg + bcolors.ENDC)
-
-def warning(obj,sub):
+import os
+def get_app_list():
+    path_dir='./testing_app'
+    file_list = os.listdir(path_dir)
+    python_list=[]
+    for file_name in file_list:
+        if finder(file_name,'.py') and not finder(file_name,'__init__'):
+            python_list.append(file_name)
