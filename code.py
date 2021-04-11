caefee6d10f1d10caa866c0ae51670bffdcc853d
root <research.csh@gmail.com>
2021-04-10 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,9 +1,15 @@
 
-import os
-def get_app_list():
-    path_dir='./testing_app'
-    file_list = os.listdir(path_dir)
-    python_list=[]
-    for file_name in file_list:
-        if finder(file_name,'.py') and not finder(file_name,'__init__'):
-            python_list.append(file_name)
+import subprocess,threading,time
+
+import os,signal
+from utils.parsing import finder
+import json
+from utils.out import bold_print,u_print,analysis
+
+
+def dy_tracing_analysis():
+    msg="\n\n"+"*"*10+" 4. Dynamic Lib 에 대한 연계 분석 "+"*"*10
+    bold_print(msg)
+    lib_dir='analysis'
+    file_list = os.listdir(lib_dir)
+    node_pool=[]
