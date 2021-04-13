37cf030b119ce73c3a53cd081a16bf5f57489f0a
root <research.csh@gmail.com>
2021-04-12 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,16 +1,1 @@
-#! /usr/python3
-# -*- coding:utf-8 -*-
-
-import os,sys
-import logging
-from pwd import getpwnam
-
-from utils.exception import CgroupsException,BASE_CGROUPS
-
-logger = logging.getLogger(__name__)
-
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logstream = logging.StreamHandler()
-logstream.setFormatter(formatter)
-logger.addHandler(logstream)
+from utils.commands import commands
