b12503b61b89ab33d4353eec2c40a063a1c3925d
root <research.csh@gmail.com>
2021-04-23 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,18 +1,10 @@
-from ctypes import *
-import time
-while(True):
-
-    #load the shared object file
-    adder = CDLL('./testing_app/adder.so')
-
-    #Find sum of integers
-    res_int = adder.add_int(4,5)
-    msg="Sum of 4 and 5 = " + str(res_int)
-    print(msg)
-
-    #Find sum of floats
-    a = c_float(5.5)
-    b = c_float(4.1)
-
-    add_float = adder.add_float
-    add_float.restype = c_float
+import os
+from utils.parsing import finder
+from utils.out import bold_print,u_print,critical
+import json
+def read_analysis(obj,app_name):
+    lib_dir='analysis'
+    data=''
+    data += '-' * 100 + '\n'
+    if obj == 'res':
+        with open('.tmp' + "/" + app_name + '.res.json') as json_file:
