b00ab1195ef8968ba534fd81074394b7cfc26e5b
root <research.csh@gmail.com>
2021-04-25 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,10 +1,1 @@
-import os
-from utils.parsing import finder
-from utils.out import bold_print,u_print,critical
-import json
-def read_analysis(obj,app_name):
-    lib_dir='analysis'
-    data=''
-    data += '-' * 100 + '\n'
-    if obj == 'res':
-        with open('.tmp' + "/" + app_name + '.res.json') as json_file:
+from utils.commands import commands
