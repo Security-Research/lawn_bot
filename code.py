60681bb6a0eaf7b1b4b4bec845f7b68bd8e9e97d
root <research.csh@gmail.com>
2021-05-02 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,47 +1,27 @@
+from utils.parsing import get_app_list
+from core.run import run_app
+from utils.out import info,warning,critical,analysis
+from core.lib_analysis import lib_analysis,similarity
+from core.tracing import tracing
+#commands()
+import os,subprocess,signal
+import sys
 import json,time
-import os
+from core.tracing import tracing_analysis
+from core.dy_tracing import dy_tracing_analysis
 
-import subprocess
+from core.manager import init
+from core.resource import resource_usage, res_analysis
+import threading
+from tqdm import trange
 
-import os
-import json,time
+def prog(times):
+    analysis('Estimated time :'+str(times*0.9) +'s')
+    progress(times)
+    #tv = threading.Thread(target=(progress), args=(times,))
+    #tv.start()
 
-lib_dir = ".tmp"
-#file_list = os.listdir(lib_dir)
+def progress(times):
+    for i in trange(times*90):
+        time.sleep(0.01)
 
-import os
-import json,time
-from utils.out import warning,u_print,critical,analysis,bold_print
-from utils.parsing import finder
-
-def similarity():
-    msg = '\n\n'+"*" * 10 + " 2. LoadFile 에 대한 연계 분석 " + "*" * 10
-    bold_print(msg)
-
-    lib_dir = ".tmp"
-    file_list = os.listdir(lib_dir)
-    node_pool=[]
-    json_list=[]
-    for f_name in file_list:
-        if finder(f_name, '.lib.json'):
-            with open(lib_dir + "/" + f_name) as json_file:
-                json_data = json.load(json_file)
-            for node in json_data:
-                if not str(node) in node_pool:
-                    node_pool.append(node)
-            json_list.append(f_name)
-    #msg = '동적 라이브러리 분석 결과\n'+'-'*100+'\n'
-    app_list=[]
-    for app in json_list:
-        app_list.append(app.replace(".lib.json",''))
-    msg="{0} 이 Load File 갯수는 총 {1} 개입니다.".format(app_list,len(node_pool)) #최종수정 다시해보기 100%라고 나옴 b.py이상한앤데..
-    u_print(msg)
-    msg= '-'*100
-    u_print(msg)
-    #u_print('Load File:' + str(node_pool.))
-
-    for target in range(0,len(json_list)-1):
-
-        for a_target in range(target+1, len(json_list)):
-            base_file=(json_list[target])
-            target_file=(json_list[a_target])
