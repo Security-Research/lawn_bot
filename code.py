b6b00ce7adad50a40d33cfdcfb181f919179b2da
root <research.csh@gmail.com>
2021-05-09 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,2 +1,20 @@
-from utils.commands import commands
+import argparse
 
+from core.report import report
+from core.manager import reset
+from core.execute import execute
+
+
+def isint(a):
+
+    if int(a):
+        return 1
+    else:
+        return 0
+
+def get_arguments():
+    return [
+    ("--start", "Start a Thermometer deamon"),
+    ("--reporter", "Get a reporter"),
+    ("--reset","Reset a Thermometer deamon")
+    ]
