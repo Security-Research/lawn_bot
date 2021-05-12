df86ac2f116af8c974fdb0784504c495f4529a07
root <research.csh@gmail.com>
2021-05-11 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,1 +1,9 @@
-
+import os
+from utils.parsing import finder
+from utils.out import bold_print,u_print,critical
+import json
+def read_analysis(obj,app_name):
+    lib_dir='analysis'
+    data=''
+    data += '-' * 100 + '\n'
+    if obj == 'res':
