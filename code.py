42825fa7f8aae52d42d0fd996112b2f3207bea91
root <research.csh@gmail.com>
2021-05-16 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,3 +1,17 @@
-from utils.parsing import get_app_list
-from core.run import run_app
-from utils.out import info,warning,critical
+
+import subprocess,threading,time
+
+import os,signal
+from utils.parsing import finder
+import json
+from utils.out import bold_print,u_print,analysis
+
+
+def dy_tracing_analysis():
+    msg="\n\n"+"*"*10+" 4. Dynamic Lib 에 대한 연계 분석 "+"*"*10
+    bold_print(msg)
+    lib_dir='analysis'
+    file_list = os.listdir(lib_dir)
+    node_pool=[]
+    json_list=[]
+    for f_name in file_list:
