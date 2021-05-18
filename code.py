b6d0159fe2dd50f05b6f9b241714be0a847792d2
root <research.csh@gmail.com>
2021-05-17 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,17 +1,16 @@
-
-import subprocess,threading,time
-
-import os,signal
-from utils.parsing import finder
-import json
-from utils.out import bold_print,u_print,analysis
+import os
+from utils.out import info,critical
 
 
-def dy_tracing_analysis():
-    msg="\n\n"+"*"*10+" 4. Dynamic Lib 에 대한 연계 분석 "+"*"*10
-    bold_print(msg)
-    lib_dir='analysis'
-    file_list = os.listdir(lib_dir)
-    node_pool=[]
-    json_list=[]
-    for f_name in file_list:
+
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+
+            os.mkdir(directory)
+            info("Created directory",directory)
+    except OSError:
+        print('Error: Creating directory. ' + directory)
+
+def removeFolder(directory):
+    try:
