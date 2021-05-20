4911da189b6bf082b829518ed87aeafabb15648a
root <research.csh@gmail.com>
2021-05-19 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,23 +1,4 @@
-import json,time
-import os
 
-import subprocess
-
-import os
-import json,time
-
-lib_dir = ".tmp"
-#file_list = os.listdir(lib_dir)
-
-import os
-import json,time
-from utils.out import warning,u_print,critical,analysis,bold_print
-from utils.parsing import finder
-
-def similarity():
-    msg = '\n\n'+"*" * 10 + " 2. LoadFile 에 대한 연계 분석 " + "*" * 10
-    bold_print(msg)
-
-    lib_dir = ".tmp"
-    file_list = os.listdir(lib_dir)
-    node_pool=[]
+import time
+for i in range(0,10000000):
+    time.sleep(0.3)
