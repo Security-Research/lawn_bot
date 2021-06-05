ceb1030626ecf784e45c5ffba0585c687196ef54
root <research.csh@gmail.com>
2021-06-04 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,11 +1,26 @@
-import os
-from utils.out import info,critical
+import argparse
+
+from core.report import report
+from core.manager import reset
+from core.execute import execute
 
 
+def isint(a):
 
-def createFolder(directory):
-    try:
-        if not os.path.exists(directory):
+    if int(a):
+        return 1
+    else:
+        return 0
 
-            os.mkdir(directory)
-            info("Created directory",directory)
+def get_arguments():
+    return [
+    ("--start", "Start a Thermometer deamon"),
+    ("--reporter", "Get a reporter"),
+    ("--reset","Reset a Thermometer deamon")
+    ]
+
+
+def add_arguments():
+    parser=argparse.ArgumentParser(description="Thermometer by syscore")
+    for argument in get_arguments():
+        parser.add_argument(argument[0], help=argument[1], action="store_true")
