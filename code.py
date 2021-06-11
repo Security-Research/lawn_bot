68a9716022fecf428d731c76660eaeb6b6bcd755
root <research.csh@gmail.com>
2021-06-10 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,48 +1,87 @@
-import argparse
 
-from core.report import report
-from core.manager import reset
-from core.execute import execute
+import subprocess,threading,time
 
-
-def isint(a):
-
-    if int(a):
-        return 1
-    else:
-        return 0
-
-def get_arguments():
-    return [
-    ("--start", "Start a Thermometer deamon"),
-    ("--reporter", "Get a reporter"),
-    ("--reset","Reset a Thermometer deamon")
-    ]
+import os,signal
+from utils.parsing import finder
+import json
+from utils.out import bold_print,u_print,analysis
 
 
-def add_arguments():
-    parser=argparse.ArgumentParser(description="Thermometer by syscore")
-    for argument in get_arguments():
-        parser.add_argument(argument[0], help=argument[1], action="store_true")
-    parser.add_argument("-s", action='store', dest='sec', help='running second',default=10)
+def dy_tracing_analysis():
+    msg="\n\n"+"*"*10+" 4. Dynamic Lib 에 대한 연계 분석 "+"*"*10
+    bold_print(msg)
+    lib_dir='analysis'
+    file_list = os.listdir(lib_dir)
+    node_pool=[]
+    json_list=[]
+    for f_name in file_list:
+        if finder(f_name, '.dy_trace.result'):
+            with open(lib_dir + "/" + f_name) as json_file:
+                json_data = json.load(json_file)
+                for node in json_data:
+                    if not str(node) in node_pool:
+                        node_pool.append(node)
+                json_list.append(f_name)
+        # msg = '동적 라이브러리 분석 결과\n'+'-'*100+'\n'
+    app_list=[]
+    for app in json_list:
+        app_list.append(app.replace(".dy_trace.result",''))
+    msg = "{0} 이 호출한 Dynamic Lib  갯수는 총 {1} 개입니다.".format(app_list, len(node_pool))  # 최종수정 다시해보기 100%라고 나옴 b.py이상한앤데..
+    u_print(msg)
+    msg = '-' * 100
+    u_print(msg)
+    u_print('dy call list:'+str(node_pool))
+    for target in range(0,len(json_list)-1):
+        for a_target in range(target+1, len(json_list)):
+            base_file=(json_list[target])
+            target_file=(json_list[a_target])
+            with open(lib_dir + "/" + base_file) as json_file:
+                base_data = json.load(json_file)
+            with open(lib_dir + "/" + target_file) as json_file:
+                target_data = json.load(json_file)
+            count=1
+            target_data_list = []
+            if (len(base_data)) < (len(target_data)):
+                for t in target_data:
+                    target_data_list.append(t)
+                for base in base_data:
+                    if base in (target_data_list):
+                        count += 1
+                percent=count/len(target_data)*100
+            else:
+                for t in base_data:
+                    target_data_list.append(t)
+                for base in target_data:
+                    if base in (target_data_list):
+                        count += 1
 
-    return parser.parse_args()
+                percent=count/len(base_data)*100
+            msg="{0} 과 {1} 앱의 Dynamic Lib 유사도는 {2} % 입니다.".format(base_file.replace(".dy_trace.result",''),target_file.replace(".dy_trace.result",''),round(percent,2))
+            analysis(msg)
 
-def commands():
-    args = add_arguments()
-    sec=args.sec
-    if args.start:
-        if not isint(sec):
-            print("sec is int")
-        #print("start",sec)
-        execute(sec)
-        #start(sec)
-    elif args.reporter:
-        #print("reporter")
-        report()
-        #report()
-    elif args.reset:
-        reset()
-        #print("reset")
-        #reset()
-    else:
+    u_print('-' * 100 + '\n')
+    pass
+
+
+
+
+def get_dy_call_list(name):
+    syscall_list=[]
+    with open('./.tmp/'+str(name), 'r') as r:
+        while(True):
+            try:
+                data=r.readline()
+                data=str(data).split(' ')
+                data=(data[1].split('('))
+                syscall_name=data[0]
+                if syscall_name not in syscall_list:
+                    syscall_list.append(syscall_name)
+                if data=='':
+                    break
+            except:
+                break
+    return (syscall_list)
+
+
+def ltrace(app_name,pid,kill_time):
+    pid=int(pid)+2
