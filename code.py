efecc1c96920fb6adb5ab3f78d8dc0e56447ab5c
root <research.csh@gmail.com>
2021-06-19 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -15,27 +15,3 @@ from core.resource import resource_usage, res_analysis
 import threading
 from tqdm import trange
 
-def prog(times):
-    analysis('Estimated time :'+str(times*0.9) +'s')
-    progress(times)
-    #tv = threading.Thread(target=(progress), args=(times,))
-    #tv.start()
-
-def progress(times):
-    for i in trange(times*90):
-        time.sleep(0.01)
-
-
-def execute(sec):
-    init()
-    app_list = (get_app_list())
-    # testing_app 에 있는 파일을 .py를 짤라서 cgroups에 등록해줘야함 중복될 수 있으니.. 잘 정리 #
-    for app_name in app_list:
-        pid = run_app(app_name,sec)
-        time.sleep(0.5)
-        info("Executed","Appname - {0} ({1}) ".format(app_name,pid))
-        t = threading.Thread(target=resource_usage,args=(app_name,pid,))
-        t.start()
-        #resource_usage(app_name,pid)
-
-    prog(30)
