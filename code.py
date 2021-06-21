67c82e50ad8f846da9f54a6099495f3f83ae0ae0
root <research.csh@gmail.com>
2021-06-20 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,17 +1,12 @@
-from utils.parsing import get_app_list
-from core.run import run_app
-from utils.out import info,warning,critical,analysis
-from core.lib_analysis import lib_analysis,similarity
-from core.tracing import tracing
-#commands()
-import os,subprocess,signal
-import sys
-import json,time
-from core.tracing import tracing_analysis
-from core.dy_tracing import dy_tracing_analysis
 
-from core.manager import init
-from core.resource import resource_usage, res_analysis
-import threading
-from tqdm import trange
+import os
+def get_app_list():
+    path_dir='./testing_app'
+    file_list = os.listdir(path_dir)
+    python_list=[]
+    for file_name in file_list:
+        if finder(file_name,'.py') and not finder(file_name,'__init__'):
+            python_list.append(file_name)
+    return (python_list)
 
+def finder(target,ob):
