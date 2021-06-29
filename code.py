d291a2328d0f31e3f8217b33400e5498a5a75c96
root <research.csh@gmail.com>
2021-06-28 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,45 +1,29 @@
-from utils.parsing import get_app_list
-from core.run import run_app
-from utils.out import info,warning,critical
-from core.cgroups import Cgroup
-from utils.out import warning,u_print,critical,analysis,bold_print
+import threading
 import os
-from utils.parsing import finder
 import time
 import json
 
 
-def resource_usage(app_name,pid):
-
-    cg=Cgroup(app_name)
-    cg.add_process(app_name,pid)
-    usage_list={'cache':[], 'rss':[],'usage':[], 'failcnt':[],'pgpgin':[], 'pgpgout':[]}
-
-    cnt =0
-    for i in range(10):
-        usage_list['cache'].append(cg.get_memory_stat(app_name,'cache')) #byte
-        usage_list['rss'].append(cg.get_memory_stat(app_name,'rss')) #byte
-        usage_list['usage'].append(cg.get_memory_info(app_name,'usage_in_bytes')) #byte
-        usage_list['failcnt'].append(cg.get_memory_info(app_name, 'usage_in_bytes')) #cnt
-        usage_list['pgpgin'].append(cg.get_memory_stat(app_name,'pgpgin')) #cnt
-        usage_list['pgpgout'].append(cg.get_memory_stat(app_name,'pgpgout')) #cnt
-        cnt += 1
-        time.sleep(1)
-    usage_list['max_usage']=cg.get_memory_info(app_name,'max_usage_in_bytes')
-
-    #print(usage_list)
-
-    with open(".tmp/" + app_name + ".res.json", "w", encoding='UTF-8') as json_file:
-        json.dump(usage_list, json_file)
-
-    #json
-    res_dic = {}
-    for key in usage_list.keys():
-        if key=='cache' or key=='rss' or key=='usage':
-            tmp=sum(usage_list[key])//cnt//1024
-        elif key=='max_usage':
-            tmp=usage_list[key]//1024
-        else:
-            tmp=sum(usage_list[key])//cnt
-        res_dic[key] = tmp
-    #print(res_dic)
+import os
+from utils.parsing import finder
+import json
+from utils.out import u_print,analysis,bold_print
+from core.dy_tracing import ltracing
+def tracing_analysis():
+    msg='\n\n'+"*"*10+" 3. System call에 대한 연계 분석 "+"*"*10
+    bold_print(msg)
+    lib_dir='analysis'
+    file_list = os.listdir(lib_dir)
+    node_pool=[]
+    json_list=[]
+    for f_name in file_list:
+        if finder(f_name, '.syscall.result'):
+            with open(lib_dir + "/" + f_name) as json_file:
+                json_data = json.load(json_file)
+                for node in json_data:
+                    if not str(node) in node_pool:
+                        node_pool.append(node)
+                json_list.append(f_name)
+        # msg = '동적 라이브러리 분석 결과\n'+'-'*100+'\n'
+    app_list=[]
+    for app in json_list:
