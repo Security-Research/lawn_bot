1b29a68c982b30647132f698fac347bd81d4ad74
root <research.csh@gmail.com>
2021-07-09 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,30 +1,65 @@
-import os
-from utils.parsing import finder
-from utils.out import bold_print,u_print,critical
-import json
-def read_analysis(obj,app_name):
-    lib_dir='analysis'
-    data=''
-    data += '-' * 100 + '\n'
-    if obj == 'res':
-        with open('.tmp' + "/" + app_name + '.res.json') as json_file:
-            json_data = json.load(json_file)
-        for v in json_data:
-            name = v
-            msg="{0} - {1}\n".format(name,json_data[name])
-            data+=msg
-    if obj=='file':
-        with open(lib_dir + "/" + app_name+'.lib.result') as json_file:
-            data=json_file.read(1000200000)
-    if obj=='system':
-        with open('.tmp' + "/" + app_name+'.syscall') as json_file:
-            data=json_file.read(1000200000)
-    if obj == 'dy':
-        with open('.tmp' + "/" + app_name + '.ltrace') as json_file:
-            data = json_file.read(1000200000)
+import subprocess, getpass, os
+from pwd import getpwnam
+from utils.exception import CgroupsException,BASE_CGROUPS
+from core.user import create_cgroups, get_user_info
+import logging
 
-    if obj=='all':
-        data += "*" * 10 + " 1.Resource" + "*" * 10+'\n'
-        data+='-'*100+'\n'
-        with open('.tmp' + "/" + app_name + '.res.json') as json_file:
-            json_data = json.load(json_file)
+CONTROL = [
+    'cpu',
+    'memory',
+]
+
+logger = logging.getLogger(__name__)
+CPU_DEFAULT = 1024
+MEMORY_DEFAULT = 1
+
+
+class Cgroup(object):
+    def __init__(self, cgroups_name, target='all', user='syscore'):
+        self.cgroups_name = cgroups_name
+        self.user = user
+        self.target = ''
+        self.u_cgroups = {}
+        self.target = [c for c in target if c in CONTROL]
+        system = os.listdir(BASE_CGROUPS)
+
+        for h in self.target:  #
+            if h not in system:
+                exit(-1)
+            user_cgroup = os.path.join(BASE_CGROUPS, h, self.user)
+            self.u_cgroups[h] = user_cgroup
+        create_cgroups(cgroups_name, script=False)
+        self.cgroups = {}
+
+        for h, user_cgroup in self.u_cgroups.items():
+            cgroup = os.path.join(user_cgroup, self.cgroups_name)
+            if not os.path.exists(cgroup):
+                os.mkdir(cgroup)
+            self.cgroups[h] = cgroup
+
+    def _get_file(self, hierarchy, file_name):
+        return os.path.join(self.cgroups[hierarchy], file_name)
+
+    def set_cpu_limit(self, limit=None):
+        if 'cpu' in self.cgroups:
+            value = self._format_cpu_value(limit)
+            cpu_shares_file = self._get_file('cpu', 'cpu.shares')
+            with open(cpu_shares_file, 'w+') as f:
+                f.write('%s\n' % value)
+        else:
+            exit(-1)
+
+    #memory
+    def get_memory_info(self,app_name,info):
+        usage = 0
+        with open('/sys/fs/cgroup/memory/' + str(app_name) + '/memory.'+info, 'r+') as f:
+            data = f.readline()
+            usage = int(data)
+        return usage
+
+    def get_memory_stat(self,app_name,info):
+        f=open('/sys/fs/cgroup/memory/' + str(app_name) + '/memory.stat', 'r+')
+        lines=f.readlines()
+        for line in lines:
+            line=line.strip('\n')
+            data=line.split(' ')
