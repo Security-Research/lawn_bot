8608e98fdfbf85372fa58d3a6b2671521846c049
root <research.csh@gmail.com>
2021-07-11 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,11 +1,4 @@
-from ctypes import *
+
 import time
-while(True):
 
-    #load the shared object file
-    adder = CDLL('./testing_app/adder.so')
-
-    #Find sum of integers
-    res_int = adder.add_int(4,5)
-    msg="Sum of 4 and 5 = " + str(res_int)
-    print(msg)
+for i in range(0,10000000):
