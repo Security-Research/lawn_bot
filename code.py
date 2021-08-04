0ee2e0b8573e46f9371235365c9048b09c592b3a
root <research.csh@gmail.com>
2021-08-03 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,34 +1,20 @@
 import os
-import subprocess
-support_interpreter='/usr/bin/python3'
-import threading
-import time
-from utils.out import info,warning,critical
-from core.lib_analysis import get_lib
-from core.tracing import tracing
-from core.dy_tracing import ltracing
-kill_time=10
-
-def poll(target,pg):
-    #warning("Analysis",""+str(target))
-    while pg.poll() == None:
-        out = pg.stdout.readline()
-        time.sleep(kill_time)
-        #critical("kill",pg.pid)
-        #pg.kill()
-        break
-
-
-def run_app(target,sec):
-    kill_time=sec
-    cmd=support_interpreter+' testing_app/'+str(target)
-    # os.popen()#예정 #get pid
-    pg = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                          stderr=subprocess.STDOUT,
-                          universal_newlines=True, shell=True)
-    t = threading.Thread(target=(poll), args=(target,pg,))
-    t.start()
-    get_lib(target, pg.pid)
-    tracing(target,pg.pid,kill_time)
-
-
+from utils.parsing import finder
+from utils.out import bold_print,u_print,critical
+import json
+def read_analysis(obj,app_name):
+    lib_dir='analysis'
+    data=''
+    data += '-' * 100 + '\n'
+    if obj == 'res':
+        with open('.tmp' + "/" + app_name + '.res.json') as json_file:
+            json_data = json.load(json_file)
+        for v in json_data:
+            name = v
+            msg="{0} - {1}\n".format(name,json_data[name])
+            data+=msg
+    if obj=='file':
+        with open(lib_dir + "/" + app_name+'.lib.result') as json_file:
+            data=json_file.read(1000200000)
+    if obj=='system':
+        with open('.tmp' + "/" + app_name+'.syscall') as json_file:
