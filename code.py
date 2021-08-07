5d7256363a97c33b2bfbace1c22c5bacfffde0fa
root <research.csh@gmail.com>
2021-08-06 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,8 +1,4 @@
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
+import time
+
+for i in range(0,10000000):
