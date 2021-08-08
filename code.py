1b097e06ebe5e5315438415b574724a3b1ab2ae1
root <research.csh@gmail.com>
2021-08-07 21:51:02 -0700

    test

--- code.py
+++ code.py
@@ -1,4 +1,15 @@
-
+from ctypes import *
 import time
+while(True):
 
-for i in range(0,10000000):
+    #load the shared object file
+    adder = CDLL('./testing_app/adder.so')
+
+    #Find sum of integers
+    res_int = adder.add_int(4,5)
+    msg="Sum of 4 and 5 = " + str(res_int)
+    print(msg)
+
+    #Find sum of floats
+    a = c_float(5.5)
+    b = c_float(4.1)
