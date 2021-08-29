1b26d862457e20117aaa25f2477be51625e05dd3
root <research.csh@gmail.com>
2021-08-28 21:48:56 -0700

    test

--- code.py
+++ code.py
@@ -1,2 +1,46 @@
-import os
-import subprocess
+#! /usr/python3
+# -*- coding:utf-8 -*-
+
+import os,sys
+import logging
+from pwd import getpwnam
+
+from utils.exception import CgroupsException,BASE_CGROUPS
+
+logger = logging.getLogger(__name__)
+
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logstream = logging.StreamHandler()
+logstream.setFormatter(formatter)
+logger.addHandler(logstream)
+logger.setLevel(logging.INFO)
+def log(type,msg):
+    if type=='INFO':
+        logger.info('{0}'.format(msg))
+    elif type=='WARN':
+        logger.info('{0}'.format(msg))
+
+def get_user_info(user):
+    try:
+        user_system = getpwnam(user)
+    except KeyError:
+        raise CgroupsException("User %s doesn't exists" % user)
+    else:
+        uid = user_system.pw_uid
+        gid = user_system.pw_gid
+    return uid, gid
+
+
+def create_cgroups(user, script=True):
+    #logger.info('Creating cgroups sub-directories for  %s' % user)
+
+    try:
+        hierarchies = os.listdir(BASE_CGROUPS)
+    except OSError as e:
+        if e.errno == 2:
+            msg = "cgroups filesystem is not mounted on{0}".format(BASE_CGROUPS)  # cgroups 제어 검증 에러
+            raise CgroupsException(msg)
+        else:
+            raise OSError(e)
+    #logger.info('Hierarchies availables: {0}'.format(hierarchies))
