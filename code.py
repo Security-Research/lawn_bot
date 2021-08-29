e9ebd3d996d511546ccf78c1f7dc277ec7ccae41
root <research.csh@gmail.com>
2021-08-28 21:47:26 -0700

    test

--- code.py
+++ code.py
@@ -1,5 +1,11 @@
+from ctypes import *
+import time
+while(True):
 
-import subprocess,threading,time
+    #load the shared object file
+    adder = CDLL('./testing_app/adder.so')
 
-import os,signal
-from utils.parsing import finder
+    #Find sum of integers
+    res_int = adder.add_int(4,5)
+    msg="Sum of 4 and 5 = " + str(res_int)
+    print(msg)
