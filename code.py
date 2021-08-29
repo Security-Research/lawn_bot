bfd75f6ae11a6ef0bb0b5ba4c3b7b720dba9ae72
root <research.csh@gmail.com>
2021-08-28 21:47:25 -0700

    test

--- code.py
+++ code.py
@@ -1,8 +1,5 @@
-#! /usr/bin/python3
-# -*- coding:utf-8 -*-
-BASE_CGROUPS = '/sys/fs/cgroup'
-HIDDEN_DIR='.data/'
 
-class CgroupsException(Exception):
-    if str(Exception).find("Exception")<0:
-        print(Exception)
+import subprocess,threading,time
+
+import os,signal
+from utils.parsing import finder
