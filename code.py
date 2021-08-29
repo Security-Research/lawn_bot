d276dfb340440863cc6ea9c07405e38b9afc27f0
root <research.csh@gmail.com>
2021-08-28 21:48:04 -0700

    test

--- code.py
+++ code.py
@@ -1,16 +1,22 @@
-#! /usr/python3
-# -*- coding:utf-8 -*-
-
-import os,sys
-import logging
+import subprocess, getpass, os
 from pwd import getpwnam
-
 from utils.exception import CgroupsException,BASE_CGROUPS
+from core.user import create_cgroups, get_user_info
+import logging
+
+CONTROL = [
+    'cpu',
+    'memory',
+]
 
 logger = logging.getLogger(__name__)
+CPU_DEFAULT = 1024
+MEMORY_DEFAULT = 1
 
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logstream = logging.StreamHandler()
-logstream.setFormatter(formatter)
-logger.addHandler(logstream)
+
+class Cgroup(object):
+    def __init__(self, cgroups_name, target='all', user='syscore'):
+        self.cgroups_name = cgroups_name
+        self.user = user
+        self.target = ''
+        self.u_cgroups = {}
