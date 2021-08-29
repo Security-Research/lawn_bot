db8fbf6d5fdc28277e41c7ebacc67db018fb7e9d
root <research.csh@gmail.com>
2021-08-28 21:48:39 -0700

    test

--- code.py
+++ code.py
@@ -1,31 +1,10 @@
-import os
-import subprocess
-support_interpreter='/usr/bin/python3'
-import threading
-import time
-from utils.out import info,warning,critical
-from core.lib_analysis import get_lib
-from core.tracing import tracing
-from core.dy_tracing import ltracing
-kill_time=10
+#! /usr/bin/python3
+# -*- coding:utf-8 -*-
+BASE_CGROUPS = '/sys/fs/cgroup'
+HIDDEN_DIR='.data/'
 
-def poll(target,pg):
-    #warning("Analysis",""+str(target))
-    while pg.poll() == None:
-        out = pg.stdout.readline()
-        time.sleep(kill_time)
-        #critical("kill",pg.pid)
-        #pg.kill()
-        break
+class CgroupsException(Exception):
+    if str(Exception).find("Exception")<0:
+        print(Exception)
 
-
-def run_app(target,sec):
-    kill_time=sec
-    cmd=support_interpreter+' testing_app/'+str(target)
-    # os.popen()#예정 #get pid
-    pg = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                          stderr=subprocess.STDOUT,
-                          universal_newlines=True, shell=True)
-    t = threading.Thread(target=(poll), args=(target,pg,))
-    t.start()
-    get_lib(target, pg.pid)
+class NamespaceException(Exception):
