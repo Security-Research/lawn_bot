13999621a8c74d8876479fa723a3ee05138f4df8
root <research.csh@gmail.com>
2021-08-28 21:48:27 -0700

    test

--- code.py
+++ code.py
@@ -1,20 +1,6 @@
-#! /usr/python3
+#! /usr/bin/python3
 # -*- coding:utf-8 -*-
+BASE_CGROUPS = '/sys/fs/cgroup'
+HIDDEN_DIR='.data/'
 
-import os,sys
-import logging
-from pwd import getpwnam
-
-from utils.exception import CgroupsException,BASE_CGROUPS
-
-logger = logging.getLogger(__name__)
-
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logstream = logging.StreamHandler()
-logstream.setFormatter(formatter)
-logger.addHandler(logstream)
-logger.setLevel(logging.INFO)
-def log(type,msg):
-    if type=='INFO':
-        logger.info('{0}'.format(msg))
+class CgroupsException(Exception):
