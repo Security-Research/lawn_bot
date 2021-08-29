6f64190c56a43bf80365121243599a2357d2d2af
root <research.csh@gmail.com>
2021-08-28 21:49:01 -0700

    test

--- code.py
+++ code.py
@@ -1,30 +1,24 @@
-import os
-from utils.out import info,critical
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
+def u_print(obj):
+    msg="{0}".format(obj)
+    print(bcolors.OKGREEN + msg + bcolors.ENDC)
 
-def createFolder(directory):
-    try:
-        if not os.path.exists(directory):
+def bold_print(obj):
+    msg="{0}".format(obj)
+    print(bcolors.BOLD + msg + bcolors.ENDC)
 
-            os.mkdir(directory)
-            info("Created directory",directory)
-    except OSError:
-        print('Error: Creating directory. ' + directory)
+def info(obj,sub):
+    msg="[Info] {0} : {1}".format(obj,sub)
+    print(bcolors.OKBLUE + msg + bcolors.ENDC)
 
-def removeFolder(directory):
-    try:
-        if os.path.exists(directory):
-            os.rmdir(directory)
-            critical("Remove directory", directory)
-    except:
-        print('Error: Removing directory. ' + directory)
-def chmod():
-    os.system('chmod 777 core/syscall')
-def init():
-    createFolder('.tmp')
-    createFolder('analysis')
-    chmod()
-
-def reset():
-    removeFolder('.tmp')
