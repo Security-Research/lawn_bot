d51e150fd7793351bf2d11579058938c43301fae
root <research.csh@gmail.com>
2021-08-28 21:48:08 -0700

    test

--- code.py
+++ code.py
@@ -1,48 +1,17 @@
-#! /usr/python3
-# -*- coding:utf-8 -*-
+import argparse
 
-import os,sys
-import logging
-from pwd import getpwnam
+from core.report import report
+from core.manager import reset
+from core.execute import execute
 
-from utils.exception import CgroupsException,BASE_CGROUPS
 
-logger = logging.getLogger(__name__)
+def isint(a):
 
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logstream = logging.StreamHandler()
-logstream.setFormatter(formatter)
-logger.addHandler(logstream)
-logger.setLevel(logging.INFO)
-def log(type,msg):
-    if type=='INFO':
-        logger.info('{0}'.format(msg))
-    elif type=='WARN':
-        logger.info('{0}'.format(msg))
-
-def get_user_info(user):
-    try:
-        user_system = getpwnam(user)
-    except KeyError:
-        raise CgroupsException("User %s doesn't exists" % user)
+    if int(a):
+        return 1
     else:
-        uid = user_system.pw_uid
-        gid = user_system.pw_gid
-    return uid, gid
+        return 0
 
-
-def create_cgroups(user, script=True):
-    #logger.info('Creating cgroups sub-directories for  %s' % user)
-
-    try:
-        hierarchies = os.listdir(BASE_CGROUPS)
-    except OSError as e:
-        if e.errno == 2:
-            msg = "cgroups filesystem is not mounted on{0}".format(BASE_CGROUPS)  # cgroups 제어 검증 에러
-            raise CgroupsException(msg)
-        else:
-            raise OSError(e)
-    #logger.info('Hierarchies availables: {0}'.format(hierarchies))
-    for hierarchy in hierarchies:
-        user_cgroup = os.path.join(BASE_CGROUPS, hierarchy, user) #경로와 제어 권한 정보를 유저명과 합침
+def get_arguments():
+    return [
+    ("--start", "Start a Thermometer deamon"),
